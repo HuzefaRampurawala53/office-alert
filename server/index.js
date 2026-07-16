1733ed1efd1d46eb3209971bcddb112a3b60238c
Your Name <huzefa.2406787109@vcet.edu.in>
2026-07-16 10:48:39 +0530

    FIxed online users

--- server/index.js
+++ server/index.js
@@ -31,8 +31,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: (origin, callback) => {
+            // Allow any origin dynamically to support localhost as well as multiple Render deployments
+            callback(null, true);
+        },
         methods: ["GET", "POST"],
+        credentials: true
     },
 });
 
